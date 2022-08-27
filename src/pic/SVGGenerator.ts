d475a35f037a8c838ef5256539c71561f901a164
jam <jmjmjmjm280@gmail.com>
2022-08-27 20:07:14 +0900

    SVGを作成するクラスの整備(rect)

---
+++ src/pic/SVGGenerator.ts
@@ -0,0 +1,49 @@
+import {RectOption, SVGOption} from "../types/SVGOptions";
+
+export class SVGGenerator {
+    public async create(
+        svgOption: SVGOption = {
+            width: 100,
+            height: 100,
+            viewBox: [0, 0, 100, 100]
+        }): Promise<Buffer> {
+        const baseSVG: string = `<svg ` +
+            `width=\"${svgOption.width}\" ` +
+            `height=\"${svgOption.height}\" ` +
+            `viewBox=\"${svgOption.viewBox.at(0)}, ` +
+                `${svgOption.viewBox.at(1)}, ` +
+                `${svgOption.viewBox.at(2)}, ` +
+                `${svgOption.viewBox.at(3)}\" ` +
+            `xmlns=\"http://www.w3.org/2000/svg\"> `;
+        const endSVG = "</svg>";
+
+        let finalSVG: string;
+
+        if(svgOption.subOptions != null) {
+            switch (svgOption.subOptions.name) {
+                case "rect":
+                    finalSVG = baseSVG + this.rectGenerator(svgOption.subOptions.option) + endSVG;
+                    break;
+                default:
+                    finalSVG = baseSVG + endSVG;
+            }
+        } else {
+            finalSVG = baseSVG + endSVG;
+        }
+        return Buffer.from(finalSVG);
+    }
+
+    private rectGenerator(rectOptions: RectOption): string {
+        return `<rect ` +
+            `x=\"${rectOptions.x}\" ` +
+            `y=\"${rectOptions.y}\" ` +
+            `width=\"${rectOptions.width}\" ` +
+            `height=\"${rectOptions.height}\" ` +
+            `rx=\"${rectOptions.rx}\" ` +
+            `ry=\"${rectOptions.rx}\" ` +
+            `fill=\"${rectOptions.fill}\" ` +
+            `fill-opacity=\"${rectOptions.fillOpacity}\"` +
+            `\>`;
+    }
+
+}
